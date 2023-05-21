a3c1372a48293788f497d44979dfd1037ddc9238
Aditya Salunke <adisalu15@gmail.com>
2023-05-21 23:15:15 +0530

    Added all validators and changed home screen:

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,22 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent],
+  imports: [IonicModule, ExploreContainerComponent,CommonModule],
 })
 export class Tab1Page {
 
-  name = 'Chaitanya Shetty';
-  rollNo = '2022510059';
+  name = 'Aditya Salunke';
+  rollNo = '2022510053';
   collegeName = 'Sardar Patel Institute of Technology';
   userImage = 'https://media.licdn.com/dms/image/D4D03AQGSx1FY-8qlVA/profile-displayphoto-shrink_400_400/0/1680951931948?e=1688601600&v=beta&t=O4dtV1IlJqr1C1Phg9j82EhTiNOKoCHoYwAKVi3Qj7M';
   cgpa = '9.5';
-  interests ='Data Science, Machine Learning, Web Development, App Development, Competitive Programming, Sports';
+  interests =' Web Development, App Development, Competitive Programming, Sports';
   hsc = '90.00 %'
   ssc = '92.40 %'
   bsc = '9.63'
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,14 +1,121 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+interface AccordionItem {
+  title: string;
+  expanded: boolean;
+}
+
+interface ListItem {
+  name: string;
+  designation: string;
+  email: string;
+  phone: string;
+  domain: string;
+  image: string;
+}
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent],
+  imports: [IonicModule, ExploreContainerComponent,CommonModule,ReactiveFormsModule]
+
 })
 export class Tab3Page {
-  constructor() {}
-}
+  registrationForm!: FormGroup;
+
+  ngOnInit() {
+    // Subscribe to value changes of relevant form controls
+    this.calculateHscPercentage();
+    this.calculateSscPercentage();
+    this.calculateOverallPercentage();
+  }
+
+  constructor(private router: Router, private formBuilder: FormBuilder) {
+    this.registrationForm = this.formBuilder.group({
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      contactNumber: ['', Validators.required],
+      rollNumber: ['', Validators.required],
+      hscCollege: ['', Validators.required],
+      hscYear: ['', Validators.required],
+      hscTotalMarks: ['', Validators.required],
+      hscMarksObtained: ['', Validators.required],
+      hscPercentage: [{ value: '', disabled: true }],
+      sscSchool: ['', Validators.required],
+      sscYear: ['', Validators.required],
+      sscTotalMarks: ['', Validators.required],
+      sscMarksObtained: ['', Validators.required],
+      sscPercentage: [{ value: '', disabled: true }],
+      cgpa: ['', Validators.required],
+      percentage: [{ value: '', disabled: true }],
+      additionalCourses: [''],
+    });
+
+    this.calculateHscPercentage();
+    this.calculateSscPercentage();
+    this.calculateOverallPercentage();
+  }
+
+  calculateHscPercentage() {
+    this.registrationForm.get('hscPercentage')!.valueChanges.subscribe(() => {
+      const totalMarks = this.registrationForm.get('hscTotalMarks')!.value;
+      const obtainedMarks = this.registrationForm.get('hscMarksObtained')!.value;
+      const percentage = (obtainedMarks / totalMarks) * 100;
+      this.registrationForm.get('hscPercentage')!.setValue(percentage.toFixed(2), { emitEvent: false });
+    });
+  }
+
+  calculateSscPercentage() {
+    this.registrationForm.get('sscPercentage')!.valueChanges.subscribe(() => {
+      const totalMarks = this.registrationForm.get('sscTotalMarks')!.value;
+      const obtainedMarks = this.registrationForm.get('sscMarksObtained')!.value;
+      const percentage = (obtainedMarks / totalMarks) * 100;
+      this.registrationForm.get('sscPercentage')!.setValue(percentage.toFixed(2), { emitEvent: false });
+    });
+  }
+
+  calculateOverallPercentage() {
+    this.registrationForm.valueChanges.subscribe(() => {
+      const hscPercentage = this.registrationForm.get('hscPercentage')!.value || 0;
+      const sscPercentage = this.registrationForm.get('sscPercentage')!.value || 0;
+      const cgpa = this.registrationForm.get('cgpa')!.value || 0;
+
+      const overallPercentage = (parseFloat(hscPercentage) + parseFloat(sscPercentage) + parseFloat(cgpa)) / 3;
+      this.registrationForm.get('percentage')!.setValue(overallPercentage.toFixed(2), { emitEvent
+        : false });
+      });
+      }
+      
+      onSubmit() {
+      // Handle form submission logic here
+      if (this.registrationForm.valid) {
+      // Form is valid, perform submission
+      console.log(this.registrationForm.value);
+      // Reset the form
+      this.registrationForm.reset();
+      // Redirect to a success page
+      this.router.navigate(['/success']);
+      } else {
+      // Form is invalid, mark all fields as touched to display validation errors
+      this.markFormGroupTouched(this.registrationForm);
+      }
+      }
+      
+      markFormGroupTouched(formGroup: FormGroup) {
+      Object.values(formGroup.controls).forEach(control => {
+      if (control instanceof FormGroup) {
+      this.markFormGroupTouched(control);
+      } else {
+      control.markAsTouched();
+      }
+      });
+      }
+      }
+      
+   
